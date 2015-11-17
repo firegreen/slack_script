f42afac85519e7dcc6579d9b4061981ad5cd8e05
unknown <steeve.v91@gmail.com>
2015-11-17 12:19:30 +0100

    alex win 2

--- alexandre_win.php
+++ alexandre_win.php
@@ -1,6 +1,6 @@
 $user_agent = "aleqsandre_win/1.0 (steeve.v91@gmail.com)";
 
-if($_POST['token'] != 'Pb7dEzfINbSANMfJaT1yx5sP'){ 
+if($_POST['token'] != 'bpejfGynnKBIs6QvgztFuLjP'){ 
     $msg = "The token for the slash command doesn't match. Check your script.";
     die($msg);
     echo $msg;
